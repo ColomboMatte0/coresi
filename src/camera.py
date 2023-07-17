72caced38b205892110869868398183696025e9b
Vincent Lequertier <vincent.lequertier@creatis.insa-lyon.fr>
2023-07-17 13:56:20 +0200

    Add TODO on camera translation

--- src/camera.py
+++ src/camera.py
@@ -25,6 +25,9 @@ class Camera(object):
 
         # Define the center of the scatterer (as if the layers would contained in a box)
         # The center is between the first and last layer
+
+        # TODO: The camera might be translated, the center might not be x: 0 and
+        # y:0
         self.sca_centre = Point(
             0.0, 0.0, (self.sca_layers[-1].center.z + self.sca_layers[0].center.z) / 2
         )
@@ -46,7 +49,7 @@ class Camera(object):
         self.Oz = Point(*position["Oz"]).normalized()
 
     def setup_scatterers(self, attrs: dict) -> list["Layer"]:
-        """The scatterer has multiple layer. For each layer, create a Layer
+        """The scatterer has multiple layers. For each layer, create a Layer
         object and sort them by their z axis"""
 
         self.n_sca_layers = attrs["n_sca_layers"]
