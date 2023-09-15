05311684f9f4b5b8c35f8930492d919f401aee84
Vincent Lequertier <vincent@vl8r.eu>
2023-09-15 13:40:05 +0200

    Use numpy array for points

--- src/point.py
+++ src/point.py
@@ -3,70 +3,45 @@ from typing import Self
 import numpy as np
 
 
-class Point(object):
-    """Class defining a 3D point with overloaded operators and norm for
-    convenience"""
+class Point(np.ndarray):
+    def __new__(cls, x, y, z):
+        """
+        :param cls:
+        """
+        obj = np.asarray([x, y, z]).view(cls)
+        return obj
 
-    def __init__(self, x: float, y: float, z: float):
-        super(Point, self).__init__()
-        self.x = x
-        self.y = y
-        self.z = z
+    @property
+    def x(self):
+        return self[0]
 
-    def __add__(self, other) -> "Point":
-        if isinstance(other, (int, float, np.int32, np.int64, np.float32, np.float64)):
-            return Point(self.x + other, self.y + other, self.z + other)
-        else:
-            return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+    @property
+    def y(self):
+        return self[1]
 
-    def __sub__(self, other) -> "Point":
-        if isinstance(other, (int, float, np.int32, np.int64, np.float32, np.float64)):
-            return Point(self.x - other, self.y - other, self.z - other)
-        else:
-            return Point(self.x - other.x, self.y - other.y, self.z - other.z)
+    @property
+    def z(self):
+        return self[2]
 
-    def __truediv__(self, other) -> "Point":
-        # Divide by constant
-        if isinstance(other, (int, float, np.int32, np.int64, np.float32, np.float64)):
-            return Point(self.x / other, self.y / other, self.z / other)
-        else:
-            # Divide coord wise
-            return Point(self.x / other.x, self.y / other.y, self.z / other.z)
+    @x.setter
+    def x(self, value):
+        self[0] = value
 
-    def __mul__(self, other) -> "Point":
-        # Multiply by constant
-        if isinstance(other, (int, float, np.int32, np.int64, np.float32, np.float64)):
-            return Point(self.x * other, self.y * other, self.z * other)
-        else:
-            # Multiply coord wise
-            return Point(self.x * other.x, self.y * other.y, self.z * other.z)
+    @y.setter
+    def y(self, value):
+        self[1] = value
 
-    def __abs__(self) -> "Point":
-        return Point(abs(self.x), abs(self.y), abs(self.z))
-
-    def __str__(self) -> str:
-        return str(vars(self))
-
-    def __repr__(self) -> str:
-        return str(vars(self))
-
-    def __eq__(self, other) -> bool:
-        return self.x == other.x and self.y == other.y and self.z == other.z
-
-    def dot(self, other) -> float:
-        return sum([self.x * other.x, self.y * other.y, self.z * other.z])
+    @z.setter
+    def z(self, value):
+        self[2] = value
 
     def normalized(self) -> "Point":
         """Normalize the point in place and return it"""
-        self.norm = float(np.linalg.norm([self.x, self.y, self.z], 2))
-        self.x = self.x / self.norm
-        self.y = self.y / self.norm
-        self.z = self.z / self.norm
-        return self
+        return self / np.linalg.norm(self, 2)
 
     def norm2(self) -> float:
         """Compute the norm2 of a point"""
-        return float(np.linalg.norm([self.x, self.y, self.z], 2))
+        return np.linalg.norm([self], 2)
 
     def get_local_coord(self, origin: Self, Ox: Self, Oy: Self, Oz: Self) -> "Point":
         """Convert the point from absolute coordinate to local from the perspective of the
--- tests/test_point.py
+++ tests/test_point.py
@@ -2,6 +2,8 @@ import os
 import sys
 import unittest
 
+import numpy as np
+
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 
 from point import Point
@@ -13,11 +15,17 @@ class TestPoint(unittest.TestCase):
         self.assertEqual(point.x, 0.2672612419124244, "Wrong x normalized")
         self.assertEqual(point.y, 0.5345224838248488, "Wrong y normalized")
         self.assertEqual(point.z, 0.8017837257372732, "Wrong z normalized")
-        self.assertEqual(point.norm, 3.7416573867739413, "Wrong norm")
 
     def test_abs(self):
         point = Point(1, 2, -3)
-        self.assertEqual(abs(point), Point(1, 2, 3), "Wrong point absolute value")
+        np.testing.assert_array_equal(abs(point), Point(1, 2, 3)),
+
+    def test_mul(self):
+        a = Point(1, 2, 3)
+        b = Point(3, 4, 5)
+        np.testing.assert_array_equal(a * 2, Point(2, 4, 6)),
+        np.testing.assert_array_equal(a**2, Point(1, 4, 9)),
+        np.testing.assert_array_equal(a * b, Point(3, 8, 15)),
 
 
 if __name__ == "__main__":
