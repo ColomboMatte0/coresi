818642d5ff173d2751a70a585af60e637324a892
Vincent Lequertier <vincent.lequertier@creatis.insa-lyon.fr>
2024-07-23 16:18:59 +0200

    Use image domain ticks labels

--- paper_images/1d_slice.py
+++ paper_images/1d_slice.py
@@ -10,34 +10,78 @@ from coresi.image import Image
 with open("../config.yaml", "r") as fh:
     config = yaml.safe_load(fh)
 
-images = ["sens-mc-poly-parallel.pth"]
-image = Image(len(config["E0"]), config["volume"])
-image.values = torch.load(images[0], map_location=torch.device("cpu"))
-print(image.values.shape)
-volume = image.values
+images = ["sens-mc-poly-parallel.pth", "sens-mc-364-angular.pth"]
+# images = ["sens-solid-attn.pth"]
 
 
 fig, axs = plt.subplots(2, 1, figsize=(10, 10))
 
-profile_h = volume[:, :, volume.shape[-2] // 2, volume.shape[-1] // 2].squeeze()
-profile_v = volume[:, volume.shape[-3] // 2, volume.shape[-2] // 2, :].squeeze()
-for e in range(profile_h.shape[0]):
-    axs[0].plot(profile_h[e], label=f"{config['E0'][e]} keV")
-    axs[0].set_xticks(list(range(0, profile_h.shape[-1], 5)))
-    axs[0].set_xticklabels(
-        list(range((-profile_h.shape[-1] // 2) + 1, profile_h.shape[-1] // 2 + 1, 5))
-    )
+for image_name in images:
+    image = Image(len(config["E0"]), config["volume"])
+    image.values = torch.load(image_name, map_location=torch.device("cpu"))
+    volume = image.values
+    volume = volume / volume.sum()
+    profile_h = volume[:, :, volume.shape[-2] // 2, volume.shape[-1] // 2]
+    profile_v = volume[:, volume.shape[-3] // 2, volume.shape[-2] // 2, :]
+    if "poly" in image_name:
+        profile_h = profile_h.squeeze()
+        profile_v = profile_v.squeeze()
+        for e in range(profile_h.shape[0]):
+            axs[0].plot(
+                torch.linspace(
+                    -config["volume"]["volume_dimensions"][0] / 2,
+                    config["volume"]["volume_dimensions"][0] / 2,
+                    config["volume"]["n_voxels"][0],
+                ),
+                profile_h[e],
+                label=f"{config['E0'][e]} keV",
+            )
 
-for e in range(profile_v.shape[0]):
-    axs[1].plot(profile_v[e], label=f"{config['E0'][e]} keV")
-    axs[1].set_xticks(list(range(0, profile_v.shape[-1], 5)))
-    axs[1].set_xticklabels(
-        list(range((-profile_v.shape[-1] // 2) + 1, profile_v.shape[-1] // 2 + 1, 5))
-    )
+            axs[1].plot(
+                torch.linspace(
+                    -config["volume"]["volume_dimensions"][2] / 2,
+                    config["volume"]["volume_dimensions"][2] / 2,
+                    config["volume"]["n_voxels"][2],
+                ),
+                profile_v[e],
+                label=f"{config['E0'][e]} keV",
+            )
+            # axs[1].set_xticks(list(range(0, profile_v.shape[-1], 5)))
+            # axs[1].set_xticklabels(
+            #    list(
+            #        range(
+            #            (-config["volume"]["volume_dimensions"][1] // 2) + 1,
+            #            config["volume"]["volume_dimensions"][1] // 2 + 1,
+            #            5,
+            #        )
+            #    )
+            # )
+    else:
+        axs[0].plot(
+            torch.linspace(
+                -config["volume"]["volume_dimensions"][0] / 2,
+                config["volume"]["volume_dimensions"][0] / 2,
+                config["volume"]["n_voxels"][0],
+            ),
+            profile_h[0],
+            label=image_name.split(".")[0].replace("_", " "),
+        )
+        axs[1].plot(
+            torch.linspace(
+                -config["volume"]["volume_dimensions"][2] / 2,
+                config["volume"]["volume_dimensions"][2] / 2,
+                config["volume"]["n_voxels"][2],
+            ),
+            profile_v[0],
+            label=image_name.split(".")[0].replace("_", " "),
+        )
+        # axs[0].plot(profile_h[0])
+
+        # axs[1].plot(profile_v[0])
+
 
 for ax in axs.ravel():
-    ax.set_yscale("log")
+    # ax.set_yscale("log")
     ax.legend(loc="upper right")
-    ax.set_title("1 d profile for " + images[0])
 plt.savefig("1d_profile_" + images[0] + ".png", dpi=300)
 plt.show()
