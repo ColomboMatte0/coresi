e4e53917cca02dfea437ce1ea94f877ac59a596a
Vincent Lequertier <vincent.lequertier@creatis.insa-lyon.fr>
2024-09-16 15:40:09 +0200

    Put the pahntom to cpu before passing it to opencv in the phantom creation tool
    Also update some paths

--- misc/phantom.py
+++ misc/phantom.py
@@ -1,19 +1,22 @@
 import cv2
 import sys
 
-sys.path.append("../coresi")
+sys.path.append("../")
 from coresi.image import Image
 import yaml
 import torch
 import matplotlib.pyplot as plt
 
-with open("config.yaml", "r") as fh:
+with open("../config.yaml", "r") as fh:
     config = yaml.safe_load(fh)
 
 phantom = Image(1, config["volume"])
 phantom.values[0, :, :, config["volume"]["n_voxels"][2] // 2] = torch.tensor(
     cv2.circle(
-        phantom.values[0, :, :, config["volume"]["n_voxels"][2] // 2].clone().numpy(),
+        phantom.values[0, :, :, config["volume"]["n_voxels"][2] // 2]
+        .cpu()
+        .clone()
+        .numpy(),
         (config["volume"]["n_voxels"][0] // 2, config["volume"]["n_voxels"][1] // 2),
         5,
         (1, 1, 1),
